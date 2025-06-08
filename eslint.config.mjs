437b5401eb9bde973f76c36c1ac8650b4f84c32b
Hossein Pourdavar <h.pourdavar@gmail.com>
2025-06-08 16:18:34 +0330

    refactor: simplify ESLint configuration using @fullstacksjs/eslint-config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,13 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from '@fullstacksjs/eslint-config';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
+/**
+ * @link https://github.com/fullstacksjs/eslint-config
+ */
+export default defineConfig({
+  jest: true,
+  typescript: {
+    tsconfigRootDir: import.meta.dirname,
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      '@typescript-eslint/no-unsafe-call': 'off',
-    },
-  },
-);
+  import: true,
+  test: true,
+});
